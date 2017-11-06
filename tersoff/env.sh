fb6f199225bdcdc16c904213af9ad6e45b6782c0
ZachMarcus <zachary.j.marcus@gmail.com>
2017-11-06 11:53:54 -0500

    Added base lammps-tersoff directory and env script

---
+++ tersoff/env.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Need Intel libraries and compilers
+source /opt/intel/parallel_studio_xe_2018/psxevars.sh
+
+# Intel Builds environment variables
+
+export KMP_BLOCKTIME=0
+export CCFLAGS="${CCFLAGS} -restrict -qopenmp -DLMP_INTEL-USELRT -DLMP_USE_MKL_RNG -O2 -fno-alias -ansi-alias -qoverride-limits -fp-model -fast=2 -no-prec-dev -xHost -DLMP_INTEL_NO_TBB"
+export LDFLAGS="${LDFLAGS} -restrict -qopenmp"
+export OMP_NUM_THREADS=64
+
+
