c39cc60dda3853378e4ebebc618dce7679ae4999
ZachMarcus <zachary.j.marcus@gmail.com>
2017-11-06 23:32:01 -0500

    Added more threading to build.sh, but the GPU package still has problems linking to mpi library

--- tersoff/lammps-tersoff-vector/machines/zach-volta/build.sh
+++ tersoff/lammps-tersoff-vector/machines/zach-volta/build.sh
@@ -44,11 +44,11 @@ ln -s nvcc_wrapper g++
 cd ../../../src
 
 if [[ ! -e lmp_kokkos_cuda_novect ]]; then
- make kokkos_cuda KOKKOS_ARCH=Maxwell50 -j16
+ make kokkos_cuda KOKKOS_ARCH=Maxwell50 -j32
  mv lmp_kokkos_cuda lmp_kokkos_cuda_novect || exit
 fi
 if [[ ! -e lmp_kokkos_omp_novect ]]; then
- make kokkos_omp -j16
+ make kokkos_omp -j32
  mv lmp_kokkos_omp lmp_kokkos_omp_novect || exit
 fi
 
@@ -61,11 +61,11 @@ cp ../../../kokkos_vector.h .
 
 
 if [[ ! -e lmp_kokkos_cuda_vect ]]; then
-  make kokkos_cuda KOKKOS_ARCH=Maxwell50 -j16
+  make kokkos_cuda KOKKOS_ARCH=Maxwell50 -j32
   mv lmp_kokkos_cuda lmp_kokkos_cuda_vect || exit
 fi
 if [[ ! -e lmp_kokkos_omp_vect ]]; then
-  make kokkos_omp -j16
+  make kokkos_omp -j32
   mv lmp_kokkos_omp lmp_kokkos_omp_vect || exit
 fi
 
@@ -76,16 +76,16 @@ make no-KOKKOS
 make yes-GPU
 function build_gpu() {
   cd ../lib/gpu
-  echo '#!/bin/bash\nmpicxx "$@"' > mpic++
-  chmod +x mpic++
+#  echo '#!/bin/bash\nmpicxx "$@"' > mpic++
+#  chmod +x mpic++
   a=$PATH
   export PATH=.:$a
   make -f Makefile.linux clean
-  make -f Makefile.linux CUDA_ARCH=-arch=sm_50 CUDA_HOME=$CUDA_ROOT CUDA_PRECISION=$1 -j16
+  make -f Makefile.linux CUDA_ARCH=-arch=sm_50 CUDA_HOME=$CUDA_ROOT CUDA_PRECISION=$1 -j32
   export PATH=$a
   cd ../../src
   make clean-mpi
-  make mpi -j16
+  make mpi -j32
   mv lmp_mpi lmp_mpi_gpu_$2 || exit
 }
 build_gpu "-D_SINGLE_SINGLE" single
