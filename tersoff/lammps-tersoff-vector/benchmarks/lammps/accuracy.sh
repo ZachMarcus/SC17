9dfa8a0b2f435eddaa26ee76960773b9baedf35d
ZachMarcus <zachary.j.marcus@gmail.com>
2017-11-10 10:17:34 -0500

    modified accuracy.sh

--- tersoff/lammps-tersoff-vector/benchmarks/lammps/accuracy.sh
+++ tersoff/lammps-tersoff-vector/benchmarks/lammps/accuracy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 
-mpirun -np 16 -host "$HOSTNAME" ../../machines/cpu-only/lammps-10Mar16/src/lmp_intel_cpu_default_vector -in in.tersoff-acc -pk intel 1 mode $1 -sf intel -v p vanilla | tee acc-$1
+mpirun -np 8 -host "$HOSTNAME" ../../machines/cpu-only/lammps-10Mar16/src/lmp_intel_cpu_default_vector -in in.tersoff-acc -pk intel 0 mode $1 -sf intel -v p vanilla | tee acc-$1
